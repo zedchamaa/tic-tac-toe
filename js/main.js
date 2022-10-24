002984cf2c45a40b41469adf31a6c02858baee2c
Zed Chamaa <sales@nineprovince.com>
2022-10-24 17:26:29 +0200

    Define the main variables

--- js/main.js
+++ js/main.js
@@ -10,5 +10,16 @@ const gameBoard = (function() {
   const turn = document.getElementById('turn');
   turn.textContent = 'X Goes First';
 
+  // define the main variables
+  let selectedBoxes = [];
+  const x_Selections = [];
+  const o_Selections = [];
+  const o_Text = 'O';
+  const x_Text = 'X';
+  let x_Score = 0;
+  let o_Score = 0;
+  let tiesScore = 0;
+  let currentPlayer = x_Text;
+
 
 })()
