9e12b893d4e666942c40196b0376ba1518f96e84
Zed Chamaa <sales@nineprovince.com>
2022-10-24 17:25:42 +0200

    Display a message that X goes first

--- js/main.js
+++ js/main.js
@@ -6,4 +6,9 @@ const gameBoard = (function() {
   // retrieve the boxes as an array  
   const boxes = Array.from(document.getElementsByClassName('box'));
   
+  // display a message that X goes first
+  const turn = document.getElementById('turn');
+  turn.textContent = 'X Goes First';
+
+
 })()
